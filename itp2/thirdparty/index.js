070a44203f4ce9e145b8578ab3c0b08eaac01bee
uliedberg <ulf@alfhild.io>
2018-08-21 00:01:44 +0200

    WIP: testing ITP2 on real domain

---
+++ itp2/thirdparty/index.js
@@ -0,0 +1,88 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  updateElWithOrigin('#origin');
+  addHasStorageAcessOnLoad({ result: '#storage-access-on-load-result' });
+  addRequestStorageAccess({ action: '#storage-access-request-action', result: '#storage-access-request-result', cookieResult: '#foo-cookie-result' });
+  addFooCookieFeature({ action: '#foo-cookie-action', result: '#foo-cookie-result' })
+});
+
+
+function updateElWithOrigin(selector) {
+  updateDomElementText(document.querySelector(selector), document.origin);
+}
+
+function addHasStorageAcessOnLoad(selectors) {
+  if (!document.hasStorageAccess) {
+    console.log('document.hasStorageAccess not available - bailing')
+    return;
+  }
+  const resultElement = document.querySelector(selectors.result);
+  document.hasStorageAccess().then(
+    function successful (hasAccess) {
+      console.log('hasStorageAccess successful, has access:', hasAccess);
+      updateDomElementText(resultElement, `successful: ${hasAccess}`);
+    },
+    function rejected (reason) {
+      console.log('hasStorageAccess rejected, reason: ', reason);
+      updateDomElementText(resultElement, `rejected: ${reason}`);
+    }
+  )
+}
+
+function addRequestStorageAccess(selectors) {
+  if (!document.requestStorageAccess) {
+    console.log('document.requestStorageAccess not available - bailing')
+    return;
+  }
+
+  const resultElement = document.querySelector(selectors.result);
+  const cookieResultElement = document.querySelector(selectors.cookieResult);
+  document.querySelector(selectors.action).addEventListener('click', function (e) {
+    e.preventDefault();
+    document.requestStorageAccess().then(
+      function () {
+        console.log('requestStorageAccess successful');
+        updateDomElementText(resultElement, 'resolved successfully');
+        const cookieVal = readJsCookie();
+        // TODO: auto-popup if no value to test if that makes a difference?
+        console.log(`trying to read cookie in successful resolved request... val: ${cookieVal}`);
+        udateDomWithCookieVal(cookieResultElement);
+      },
+      function () {
+        console.log('requestStorageAccess rejected');
+        updateDomElementText(resultElement, 'rejected');
+      }
+    )
+  });
+}
+
+function addFooCookieFeature(selectors) {
+  const resultElement = document.querySelector(selectors.result);
+  udateDomWithCookieVal(resultElement);
+  document.querySelector(selectors.action).addEventListener('click', function (e) {
+    e.preventDefault();
+    udateDomWithCookieVal(resultElement);
+  });
+}
+
+function udateDomWithCookieVal(resultElement) {
+  updateDomElementText(resultElement, readJsCookie() || 'no value in cookie');
+}
+
+function updateDomElementText(element, text) {
+  element.innerHTML = text;
+  element.classList.add('updated');
+  setTimeout(function () { element.classList.remove('updated'); }, 200);
+}
+
+// TODO: add a name arg :)
+function readJsCookie() {
+  return document.cookie.replace(/(?:(?:^|.*;\s*)foo\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+}
+
+function uuidv4 () {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
---
+++ itp2/thirdparty/popup.js
@@ -0,0 +1,62 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  updateElWithOrigin('#origin');
+  addCloseFeature('#close-window');
+  addFooCookieFeature({ actions: { read: '#foo-cookie-read-action', write: '#foo-cookie-write-action' }, result: '#foo-cookie-result' })
+});
+
+
+function updateElWithOrigin(selector) {
+  updateDomElementText(document.querySelector(selector), document.origin);
+}
+
+function addCloseFeature(selector) {
+  document.querySelector(selector).addEventListener('click', function (e) {
+    e.preventDefault();
+    window.close();
+  });
+}
+
+// TODO: DRY 😊
+function addFooCookieFeature(selectors) {
+  const resultElement = document.querySelector(selectors.result);
+  udateDomWithCookieVal(resultElement);
+  document.querySelector(selectors.actions.read).addEventListener('click', function (e) {
+    e.preventDefault();
+    udateDomWithCookieVal(resultElement);
+  });
+  document.querySelector(selectors.actions.write).addEventListener('click', function (e) {
+    e.preventDefault();
+    writeJsCookie('foo', uuidv4());
+    udateDomWithCookieVal(resultElement);
+  });
+}
+
+function udateDomWithCookieVal(resultElement) {
+  updateDomElementText(resultElement, readJsCookie() || 'no value in cookie');
+}
+
+// TODO: add a name arg :)
+function readJsCookie() {
+  return document.cookie.replace(/(?:(?:^|.*;\s*)foo\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+}
+
+function writeJsCookie(name, value) {
+  const eTldPlusOne = document.location.hostname.replace(/.*\.(.*\..*)/, '$1');
+  const cookieString = `${name}=${value};domain=${eTldPlusOne};max-age=${60*60*24*365}`;
+  console.log('cookie string about to be assigned to document.cookie: ', cookieString);
+  document.cookie = cookieString;
+}
+
+function uuidv4 () {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+function updateDomElementText(element, text) {
+  element.innerHTML = text;
+  element.classList.add('updated');
+  setTimeout(function () { element.classList.remove('updated'); }, 200);
+}
